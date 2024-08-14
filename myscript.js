b8906497bd8351372e6ad1ad87ac426236d0050a
ThatCombatWombat <aldenharrison100@gmail.com>
2024-08-14 18:58:34 -0400

    added app

---
+++ myscript.js
@@ -0,0 +1,60 @@
+function validateForm() {
+    // Get the value of the input fields
+    let l = document.getElementById("length").value;
+    let h = document.getElementById("height").value;
+    let lb = document.getElementById("long").value;
+    let sb = document.getElementById("short").value;
+    let m = document.getElementById("mass").value;
+  
+    let text;
+  
+    // Calculate the value
+    text = m / (0.5 * (sb + lb) * h * l);
+  
+    // Display the result
+    document.getElementById("demo").innerHTML = text;
+  }
+  
+  // Add event listener to the "mass" input field
+  var input = document.getElementById("mass");
+  input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("myBtn").click();
+    }
+  });
+  
+  // Add event listener to the "length" input field
+  var length = document.getElementById("length");
+  length.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("height").focus();
+    }
+  });
+
+    // Add event listener to the "length" input field
+  var length = document.getElementById("height");
+  length.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("long").focus();
+    }
+  });
+
+    // Add event listener to the "length" input field
+  var length = document.getElementById("long");
+  length.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("short").focus();
+    }
+  });
+      // Add event listener to the "length" input field
+      var length = document.getElementById("short");
+      length.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          document.getElementById("mass").focus();
+        }
+      });
